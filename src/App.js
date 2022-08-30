8475cfda115966743bfdc3324d9119e05e7cebed
arun2604 <54104684+arun2604@users.noreply.github.com>
2022-08-30 11:43:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{useEffect} from 'react';
+import NewsList from './components/NewsList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
